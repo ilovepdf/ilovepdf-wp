981f781a615fe18e64e83ac43211bd09287be0e3
Diego Orellano <die.ore@Mac-mini.local>
2023-09-20 11:59:37 -0300

    Capabilities should be used instead of roles

--- admin/ilove-pdf-admin.php
+++ admin/ilove-pdf-admin.php
@@ -35,7 +35,7 @@ function ilove_pdf_menu() {
         'options-general.php',              // Register this submenu with the menu defined above
         'iLovePDF Settings',                // The text to the display in the browser when this menu item is active
         'iLovePDF',                         // The text for this menu item
-        'administrator',                    // Which type of users can see this menu
+        'manage_options',                    // Which type of users can see this menu
         'ilove-pdf-content-setting',        // The unique ID - the slug - for this menu item
         'ilove_pdf_content_page_setting'    // The function used to render the menu for this page to the screen
     );
@@ -44,7 +44,7 @@ function ilove_pdf_menu() {
         'upload.php',
         'iLovePDF Statistics',
         'iLovePDF',
-        'administrator',
+        'manage_options',
         'ilove-pdf-content-statistics',
         'ilove_pdf_content_page_statistics'
     );
