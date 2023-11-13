cd0de82d0ed72ceb56d574c8048e9677a8530179
Diego Orellano <die.ore@Mac-mini.local>
2023-11-13 14:53:09 -0300

    plugin update to 2.0.2

--- includes/Ilove_Pdf.php
+++ includes/Ilove_Pdf.php
@@ -74,7 +74,7 @@ class Ilove_Pdf {
 	public function __construct() {
 
 		$this->plugin_name = 'ilove-pdf';
-		$this->version     = 'wp.2.0.1';
+		$this->version     = 'wp.2.0.2';
 
 		$this->load_dependencies();
 		$this->set_locale();
