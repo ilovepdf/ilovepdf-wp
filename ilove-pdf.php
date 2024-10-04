b3b30af4272b4cb610fb842b6337f59c733b182a
Diego Orellano <die.ore@Mac-mini.local>
2024-10-04 15:58:47 -0300

    Improved comments

--- ilove-pdf.php
+++ ilove-pdf.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
+ * IlovePdf WordPress Plugin
  *
  * @link              https://ilovepdf.com/
  * @since             1.0.0
