93d7401285ea21d751ec1cc8ae438e70a9d1d21b
TeamCrombieDiego <die.ore@Mac-mini.local>
2025-08-12 12:11:04 -0300

    Removed shadow on links when focus is active.

--- assets/build/main.min.asset.php
+++ assets/build/main.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'dddd374ccbc8c50170fa');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '0d0483bc65e467964550');
