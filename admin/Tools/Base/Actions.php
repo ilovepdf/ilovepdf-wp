4991ca09a746c95925fca98df0eee8bd190fd025
MarianaCaradec <mariana.caradec@crombie.dev>
2025-11-28 11:43:29 -0300

    refactor: update error message for file processing failure

--- admin/Tools/Base/Actions.php
+++ admin/Tools/Base/Actions.php
@@ -34,7 +34,7 @@ trait Actions {
                 <svg width="14px" height="14px" aria-hidden="true" focusable="false" data-icon="circle-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"></path></svg>
                 <span class="">%1$s</span>
             </div>',
-            esc_html_x( 'Process failure', 'Error message: the file could not be processed.', 'ilove-pdf' ),
+            esc_html_x( 'Something went wrong', 'Error message: the file could not be processed.', 'ilove-pdf' ),
         );
     }
 
