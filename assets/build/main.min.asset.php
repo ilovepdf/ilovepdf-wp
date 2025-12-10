3e56356fb0d86c0148940c87531e8a6ed766e8a6
TeamCrombieMarianaCaradec <equipo.crombie@crombie.dev>
2025-12-10 12:14:20 -0300

    fix: improve setResume function to use innerHTML for resume content

--- assets/build/main.min.asset.php
+++ assets/build/main.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '705ae700926d8c0eb475');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '5442e8180d6cbaaba2c5');
