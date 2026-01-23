8f2a316de496fde4c97cb51d6f5471651aec7412
TeamCrombieMarianaCaradec <equipo.crombie@crombie.dev>
2026-01-23 09:24:19 -0300

    feat: add styles for better text handling within menu items with line height and word wrapping

--- assets/build/main.min.asset.php
+++ assets/build/main.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '0f4b149c6daf16d6ce03');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '530900c05d82c9571f92');
